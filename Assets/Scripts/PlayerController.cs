e90411020cdd1d730c7407ecfff9a7949e7fef66
LaciXO <ljosephcoble@icloud.com>
2022-04-28 15:55:20 +1000

    firstexport

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 300.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + (transform.forward * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + (transform.forward * -0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + (transform.right * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (transform.right * -0.1f);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            // turn on the cursor
+            GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+        }
+    }
+}
